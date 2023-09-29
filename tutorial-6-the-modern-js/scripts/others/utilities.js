000c7b262c922fbac33b099a1f05dd82f37649ec
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2023-09-29 08:32:08 +0600

    Did some warm-ups.

---
+++ tutorial-6-the-modern-js/scripts/others/utilities.js
@@ -0,0 +1,4 @@
+export function pagePrint (str) {
+    let para = document.getElementById(`main-print`);
+    para.innerHTML = str;
+};
---
+++ tutorial-6-the-modern-js/scripts/others/warm-ups.js
@@ -0,0 +1,5 @@
+import {pagePrint} from './utilities.js';
+
+let aString = `This is a sample line.`
+
+pagePrint(aString);
