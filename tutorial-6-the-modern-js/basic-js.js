7f32dca7bc0095a79cdc1e6b476da9bb18c03288
HasanNahiyanNobel <hasan.nahiyan.nobel@gmail.com>
2024-05-18 15:32:40 +0600

    Added a light/dark mode switcher.

--- tutorial-6-the-modern-js/basic-js.js
+++ tutorial-6-the-modern-js/basic-js.js
@@ -1,11 +1,14 @@
-/*
+/*******************************************************************************
  * Constants and Variables
- */
+ ******************************************************************************/
+const MASTER_DIV = document.getElementById(`master-div`);
 const OUTPUT_DIV = document.getElementById('output-div');
+const LIGHT_MODE = `light`;
+const DARK_MODE = `dark`;
 
-/*
+/*******************************************************************************
  * Functions
- */
+ ******************************************************************************/
 
 /**
  * Prints output as HTML.
@@ -15,6 +18,28 @@ function printOutput(output) {
   OUTPUT_DIV.innerHTML += output;
 }
 
-/*
- * Execution point of the script
+/**
+ * Sets light/dark mode when the user-device mode changes.
+ * @param colourScheme Mode to be set (light or dark).
  */
+function setColourTheme(colourScheme) {
+  if (colourScheme === DARK_MODE) {
+    MASTER_DIV.classList.add(`bg-dark`);
+    MASTER_DIV.classList.add(`text-light`);
+  } else {
+    MASTER_DIV.classList.remove(`bg-dark`);
+    MASTER_DIV.classList.remove(`text-light`);
+  }
+}
+
+/*******************************************************************************
+ * Execution Point of the Script
+ ******************************************************************************/
+
+// Listen to light/dark mode change, and set theme accordingly.
+// Concept from: https://stackoverflow.com/a/57795495
+window.matchMedia(`(prefers-color-scheme: dark)`).
+    addEventListener(`change`, event => {
+      const currentColourScheme = event.matches ? DARK_MODE : LIGHT_MODE;
+      setColourTheme(currentColourScheme);
+    });
