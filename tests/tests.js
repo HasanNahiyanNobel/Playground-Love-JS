170f1d21528a309772a6d63390a43b8c3bd8b5fc
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-11-25 21:58:37 +0600

    Updated and added `tests.js` to `index.html`.

--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,4 @@
  * This file contains basic tests to verify the functionality of the code.
  */
 
-printOutput(`<p><code>tests.js</code> loaded!</p><p>`);
-
-printOutput(`</p>`)
+printOutput(`<p><code>tests.js</code> loaded!</p>`);
