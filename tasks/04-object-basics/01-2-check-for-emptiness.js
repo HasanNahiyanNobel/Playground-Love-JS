e904b24e66febf4f858b15268b081539d9b8dad1
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-11-29 18:59:08 +0600

    Completed task 04-01-2-check-for-emptiness.

---
+++ tasks/04-object-basics/01-2-check-for-emptiness.js
@@ -0,0 +1,22 @@
+// Task: https://javascript.info/object#check-for-emptiness
+
+let schedule = {}; // An empty object.
+printOutput(isEmpty(schedule));
+printOutput(`<br>`)
+schedule[`8:30`] = `get up`; // Now the object has a property.
+printOutput(isEmpty(schedule));
+
+// Function Declaration(s)
+/**
+ * Checks if an object is empty (has no enumerable properties).
+ *
+ * @param {Object} obj - The object to check for emptiness.
+ * @returns {boolean} - Returns `true` if the object is empty, otherwise
+ * `false`.
+ */
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
