dfd894d4dc26774ff217fa5b838608d4b128a67f
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-11-29 19:19:21 +0600

    Completed task 04-01-3-sum-object-properties.

---
+++ tasks/04-object-basics/01-3-sum-object-properties.js
@@ -0,0 +1,34 @@
+// Task: https://javascript.info/object#sum-object-properties
+
+// Define the demo objects.
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+let emptySalaries = {};
+
+let sum = undefined; // Variable to store the sum of the salaries.
+
+// Print sum of the salaries.
+sum = sumOfTheSalaries(salaries);
+printOutput(`Sum of the salaries: ${sum}<br>`);
+sum = sumOfTheSalaries(emptySalaries);
+printOutput(`Sum of the empty salaries: ${sum}`);
+
+// Function Definition(s)
+/**
+ * Calculates the sum of all salaries in the given object.
+ *
+ * @param {Object} salaries - An object where keys are employee names and values
+ * are their salaries.
+ * @returns {number} - The sum of all salaries.
+ */
+function sumOfTheSalaries(salaries) {
+  let sum = 0;
+  for (let key in salaries) {
+    sum += salaries[key];
+  }
+  return sum;
+}
