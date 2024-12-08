bb40266a17d349768e830996c0011bdb6b668207
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2024-12-08 17:41:02 +0600

    Rewrote task 05-1-translate-border-left-width-to-borderleftwidth.

--- tasks/05-data-types/05-1-translate-border-left-width-to-borderleftwidth.js
+++ tasks/05-data-types/05-1-translate-border-left-width-to-borderleftwidth.js
@@ -1,8 +1,16 @@
 // Task: https://javascript.info/array-methods#translate-border-left-width-to-borderleftwidth
 
-console.log(camelize(`background-color`));
-console.log(camelize(`list-style-image`));
-console.log(camelize(`-webkit-transition`));
+console.log(camelize(`three-words-here`));
+console.log(camelize(`two-words`));
+console.log(camelize(`one`));
+console.log(camelize(``));
+console.log(camelize(`-hyphen-before`));
+console.log(camelize(`hyphen-after-`));
+console.log(camelize(`-hyphen-before-and-after-`));
+console.log(camelize(`-`));
+console.log(camelize(`--`));
+console.log(camelize(`-before`));
+console.log(camelize(`after-`));
 
 /**
  * Converts a dash-separated string to camelCase.
@@ -11,10 +19,18 @@ console.log(camelize(`-webkit-transition`));
  * @returns {string} - The camelCase version of the string.
  */
 function camelize(str) {
-  return str
-    .split(`-`)
-    .map((word, index) =>
-      index === 0 ? word : word[0].toUpperCase() + word.slice(1)
-    )
-    .join(``);
+  // Split the string by dashes.
+  let wordsOfStr = str.split(`-`);
+
+  // If there is only one word, return the string as it is.
+  if (wordsOfStr.length === 1) return str;
+
+  // Capitalize the first letter of each word except the first word and empty
+  // words.
+  wordsOfStr = wordsOfStr.map((word, index) => {
+    if (index === 0 || word === ``) return word;
+    return word[0].toUpperCase() + word.slice(1);
+  });
+
+  return wordsOfStr.join(``);
 }
